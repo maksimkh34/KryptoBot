ceea97882e1c2365d999a205ea9ce582ccc9be7f
maksimkh34 - DSKTPL <maksimkh34@duck.com>
2025-05-22 10:03:53 +0300

    round byn to xx.50 or xx.00

--- src/data/utils.py
+++ src/data/utils.py
@@ -1,13 +1,15 @@
 from decimal import Decimal, ROUND_UP
 
-def round_byn(amount):
+def round_byn(amount, base=5):
     """
     Округляет сумму в BYN вверх до двух знаков после запятой.
 
     Args:
         amount (float или Decimal): Сумма для округления.
+        :param base:
+        :param amount:
 
     Returns:
         Decimal: Округленная сумма.
     """
-    return Decimal(amount).quantize(Decimal('0.01'), rounding=ROUND_UP)
+    return base * Decimal(amount / base).quantize(Decimal('0.1'), rounding=ROUND_UP)
