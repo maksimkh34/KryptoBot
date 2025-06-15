c60cda98135613bb49943613b89e0fed50053cbe
maksimkh34 - DSKTPL <maksimkh34@duck.com>
2025-06-16 00:57:07 +0300

    wallets fix

--- src/core/crypto/tron/TronManager.py
+++ src/core/crypto/tron/TronManager.py
@@ -47,11 +47,17 @@ class TronManager:
         return None
 
     def pay(self, address: str, amount: Amount) -> PayResult:
-        no_fees_wallets = self.get_no_fees_wallets()
+        _wallets = []
+
+        for wallet in self.storage.data:
+            if self.client.get_balance(wallet.get_address()) > amount.get_to_trx():
+                _wallets.append(wallet)
+
+        no_fees_wallets = self.get_no_fees_wallets(_wallets)
         fee = False
 
         if len(no_fees_wallets) == 0:
-            wallet = self.get_wallet_with_lower_reminder(self.storage.data, amount)
+            wallet = self.get_wallet_with_lower_reminder(_wallets, amount)
             fee = True
         else:
             wallet = self.get_wallet_with_lower_reminder(no_fees_wallets, amount)
@@ -62,15 +68,15 @@ class TronManager:
         self.client.transfer(wallet.get_private_key(), address, amount)
         return PayResult.COMPLETED_FEE if fee else PayResult.COMPLETED
 
-    def get_no_fees_wallets(self):
+    def get_no_fees_wallets(self, wallets):
         no_fees_wallets = []
-        for wallet in self.storage.data:
+        for wallet in wallets:
             if self.client.can_transfer_without_fees(wallet.get_address()):
                 no_fees_wallets.append(wallet)
         return no_fees_wallets
 
     def can_transfer_without_fees(self):
-        return len(self.get_no_fees_wallets()) > 0
+        return len(self.get_no_fees_wallets(self.storage.data)) > 0
 
     def get_max_payment_amount(self):
         max_balance = -1
